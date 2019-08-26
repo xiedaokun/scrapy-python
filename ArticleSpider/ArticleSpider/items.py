d322584d3a52ec0a7e421c0714746dcafb9443d7
xiedaokun <102251868@qq.com>
2019-08-27 02:40:54 +0800

    t

--- ArticleSpider/ArticleSpider/items.py
+++ ArticleSpider/ArticleSpider/items.py
@@ -12,3 +12,19 @@ class ArticlespiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+class JobBoleArticleItem(scrapy.Item):
+    # 封面图
+    front_image_url = scrapy.Field()
+    first_image = scrapy.Field()
+    # 封面图路径
+    front_image_path = scrapy.Field()
+    title = scrapy.Field()
+    content = scrapy.Field()
+    url = scrapy.Field()
+    give_sums = scrapy.Field()
+    download_nums = scrapy.Field()
+    source_letters = scrapy.Field()
+    update_time = scrapy.Field()
+    subtitle_size = scrapy.Field()
