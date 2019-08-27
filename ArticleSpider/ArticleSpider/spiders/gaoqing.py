b226eb58703d748831c26b038ada63977d82fd14
xiedaokun <102251868@qq.com>
2019-08-27 11:27:53 +0800

    t

---
+++ ArticleSpider/ArticleSpider/spiders/gaoqing.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class GaoqingSpider(scrapy.Spider):
+    name = 'gaoqing'
+    allowed_domains = ['gaoqing.fm']
+    start_urls = ['http://gaoqing.fm/']
+
+    def parse(self, response):
+        pass
