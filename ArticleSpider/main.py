b31008ed89d543ba651f3828757215176c5bcdcd
xiedaokun <102251868@qq.com>
2019-08-27 11:44:06 +0800

    t

--- ArticleSpider/main.py
+++ ArticleSpider/main.py
@@ -3,5 +3,6 @@ import sys
 import os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-execute(["scrapy", "crawl", "jobbole"])
+# execute(["scrapy", "crawl", "jobbole"])
 # execute(["scrapy", "crawl", "image"])
+execute(["scrapy", "crawl", "gaoqing"])
