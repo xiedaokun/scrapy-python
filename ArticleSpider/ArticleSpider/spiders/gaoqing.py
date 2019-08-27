7bf9c7618e6dc7017ab00ea8a87bf88738c9144a
xiedaokun <102251868@qq.com>
2019-08-27 13:57:48 +0800

    t

--- ArticleSpider/ArticleSpider/spiders/gaoqing.py
+++ ArticleSpider/ArticleSpider/spiders/gaoqing.py
@@ -2,10 +2,11 @@
 import scrapy
 
 
-class GaoqingSpider(scrapy.Spider):
-    name = 'gaoqing'
-    allowed_domains = ['gaoqing.fm']
-    start_urls = ['http://gaoqing.fm/']
+# class GaoqingSpider(scrapy.Spider):
+#     name = 'gaoqing'
+#     allowed_domains = ['gaoqing.fm']
+#     start_urls = ['http://gaoqing.fm/']
 
-    def parse(self, response):
-        pass
+#     def parse(self, response):
+#         pass
+print(123)
