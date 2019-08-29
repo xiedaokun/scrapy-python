54f3490f540cc65d91f57a1f7a6947a3148ec4c6
xiedaokun <102251868@qq.com>
2019-08-29 19:33:10 +0800

    t

--- ArticleSpider/ArticleSpider/settings.py
+++ ArticleSpider/ArticleSpider/settings.py
@@ -67,7 +67,7 @@ ROBOTSTXT_OBEY = False
 ITEM_PIPELINES = {
     # 'scrapy.pipelines.images.ImagesPipeline': 1,
     # 'ArticleSpider.pipelines.ArticleImagePipeline': 1,
-    # 'ArticleSpider.pipelines.MysqlTwistedPipline': 2,
+    'ArticleSpider.pipelines.MysqlTwistedPipline': 2,
     # 'ArticleSpider.pipelines.GaoqingPipleline': 300,
 
 }
